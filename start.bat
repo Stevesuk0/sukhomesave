b64d53d315cfb9cd2232b26f122215b089fe92fe
Stevesuk <stevesukawa@outlook.com>
2025-07-10 16:30:26 +0800

    202507101630

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files\Eclipse Adoptium\jdk-21.0.7.6-hotspot\bin\java" -Xmx4096m -jar server.jar nogui
+"C:\Program Files\Eclipse Adoptium\jdk-21.0.7.6-hotspot\bin\java" -Dfile.encoding=UTF-8 -Xmx4096m -jar server.jar nogui
